73d39eaa5bc76d6991d7cd3b13128116814d5b1a
Logan O'Hara <taikedo@protonmail.com>
2019-06-12 21:58:24 -0400

    Refactor parser file into functions

---
+++ lazytube/__init__.py
@@ -0,0 +1,3 @@
+from . import parser
+
+print(parser.parsefile("data/example.txt"))
--- lazytube/parser.py
+++ lazytube/parser.py
@@ -1,32 +1,41 @@
 import sys
-import helpers
+from . import helpers
 
-# take file (first argument) and split into list
-try:
-    file = helpers.txttolist(sys.argv[1])
-except IndexError as e:
-    sys.exit(helpers.errormsg("No file specified"))
-except IOError as e:
-    sys.exit(helpers.errormsg("File not found"))
-except Exception as e:
-    sys.exit(helpers.errormsg(e))
+def filetolist(filepath):
+    """Takes file (first argument) and split into list"""
+    try:
+        return helpers.txttolist(filepath)
+    except IndexError as e:
+        sys.exit(helpers.errormsg("No file specified"))
+    except IOError as e:
+        sys.exit(helpers.errormsg("File not found"))
+    except Exception as e:
+        sys.exit(helpers.errormsg(e))
 
-# seperate list into groups of header and body
-i = 0
-slidedata = [[]]
-for line in file:
-    if line != "":
-        slidedata[i].append(line)
-        continue
-    i += 1
-    slidedata.append([])
+def listtogroups(filelist):
+    """Seperates list into groups of header and body"""
+    i = 0
+    slidedata = [[]]
+    for line in filelist:
+        if line != "":
+            slidedata[i].append(line)
+            continue
+        i += 1
+        slidedata.append([])
+    return slidedata
 
-# check if format and length is correct + add img and durration elements
-slides = []
-for slide in slidedata:
-    slide = slide[:2]
-    slide[0] = slide[0][:25]
-    slide[1] = slide[1][:140]
-    slide.append("path")
-    slide.append(0)
-    slides.append(slide)
+def listtoslides(data):
+    """Checks if format is correct + adds img and durration elements"""
+    slides = []
+    for slide in data:
+        slide = slide[:2]
+        slide[0] = slide[0][:25]
+        slide[1] = slide[1][:140]
+        slide.append("imgpath")
+        slide.append(0)
+        slides.append(slide)
+    return slides
+
+def parsefile(file):
+    """Parses file into slides"""
+    return listtoslides(listtogroups(filetolist(file)))
