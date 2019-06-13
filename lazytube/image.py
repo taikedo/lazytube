a20fbb76f754b4c6213b8614e843450ffc74da85
Logan O'Hara <taikedo@protonmail.com>
2019-06-13 01:37:06 -0400

    Update offset amount

--- lazytube/image.py
+++ lazytube/image.py
@@ -8,10 +8,13 @@ def createslide(width, height, path, name, slide):
     img = Image.new("RGB", (width, height))
     draw = ImageDraw.Draw(img)
 
-    draw.text((25, 25), slide[0], (255, 255, 255), font=font1)
-    draw = ImageDraw.Draw(img)
+    offset = 25
+    for line in textwrap.wrap(slide[0], width = 18):
+        draw.text((25, offset), line, (255, 255, 255), font=font1)
+        draw = ImageDraw.Draw(img)
+        offset += 82
 
-    offset = 150
+    offset += 20
     for line in textwrap.wrap(slide[1], width = 23):
         draw.text((25, offset), line, (255, 255, 255), font=font2)
         draw = ImageDraw.Draw(img)
